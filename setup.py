3947622b5bd690c9650ce542504a9524399184cb
tiago <tiago@marketpsychdata.com>
2021-07-05 10:29:45 +0800

    updated to version 0.0.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
   name='marketpsych',
   packages=['marketpsych'],
-  version='0.0.6',
+  version='0.0.7',
   author='MarketPsych Data',
   description='Python libraries for working with MarketPsych\'s feeds',
   long_description=long_description,
